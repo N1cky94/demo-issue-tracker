cc921f82f6e3c3cf28f90e84517eb57c81d4b02a
Nicky <nick.bauters@me.com>
2024-10-04 10:18:31 +0200

    tests: Add "integration-test" to Context load test

--- src/test/java/be/archilios/issues/issuetracker/IssueTrackerApplicationTests.java
+++ src/test/java/be/archilios/issues/issuetracker/IssueTrackerApplicationTests.java
@@ -1,9 +1,11 @@
 package be.archilios.issues.issuetracker;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Tag("integration-tests")
 class IssueTrackerApplicationTests {
     
     @Test
