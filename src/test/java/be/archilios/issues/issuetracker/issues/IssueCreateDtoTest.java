04767be2a84499c3a191facb639b686ff672d047
Nicky <nick.bauters@me.com>
2024-10-04 12:53:58 +0200

    test: Add createDto test to check for valid and invalid type

---
+++ src/test/java/be/archilios/issues/issuetracker/issues/IssueCreateDtoTest.java
@@ -0,0 +1,37 @@
+package be.archilios.issues.issuetracker.issues;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IssueCreateDtoTest {
+    
+    @Test
+    @DisplayName("GIVEN valid dto WHEN requested to map to domain model THEN mapping happens correct")
+    void givenValidDto_whenRequestedToMapToDomainModel_thenMappingHappensCorrect() {
+        // given
+        IssueCreateDto dto = new IssueCreateDto("Severe breakdown", "SEVERE");
+        
+        // when
+        Issue issue = dto.toIssue();
+        
+        // then
+        assertNotNull(issue);
+        assertNull(issue.getId());
+        assertEquals("Severe breakdown", issue.getName());
+        assertEquals("Nick", issue.getAssignee());
+        assertEquals(IssueType.SEVERE, issue.getType());
+    }
+    
+    @Test
+    @DisplayName("GIVEN invalid type WHEN request for mapping happens THEN exception is thrown")
+    void givenInvalidType_whenRequestForMappingHappens_thenExceptionIsThrown() {
+        // given
+        IssueCreateDto dto = new IssueCreateDto("Severe breakdown", "INVALID");
+        
+        // when
+        assertThrows(IllegalArgumentException.class, dto::toIssue);
+    }
+    
+}
