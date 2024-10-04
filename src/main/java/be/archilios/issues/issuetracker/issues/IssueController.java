097392436ea90b55d2561226be5e1cff9b191f32
Nicky <nick.bauters@me.com>
2024-10-04 13:29:23 +0200

    feat: Make the POST request dynamic

--- src/main/java/be/archilios/issues/issuetracker/issues/IssueController.java
+++ src/main/java/be/archilios/issues/issuetracker/issues/IssueController.java
@@ -1,10 +1,7 @@
 package be.archilios.issues.issuetracker.issues;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,12 +19,7 @@ public class IssueController {
     
     @PostMapping
     public void createIssue(IssueCreateDto issueCreateDto) {
-//        Issue issue = issueCreateDto.toIssue();
-        Issue issue = new Issue(
-                "Severe breakdown",
-                "Nick",
-                IssueType.SEVERE
-        );
+        Issue issue = issueCreateDto.toIssue();
         issueRepo.save(issue);
     }
 }
