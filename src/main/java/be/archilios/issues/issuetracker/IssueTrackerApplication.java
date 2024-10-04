03bd8ff15cad861c32b31c409f716c69744f216a
Nicky <nick.bauters@me.com>
2024-10-04 09:53:02 +0200

    Initialize project

---
+++ src/main/java/be/archilios/issues/issuetracker/IssueTrackerApplication.java
@@ -0,0 +1,13 @@
+package be.archilios.issues.issuetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueTrackerApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(IssueTrackerApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/be/archilios/issues/issuetracker/IssueTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package be.archilios.issues.issuetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueTrackerApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
